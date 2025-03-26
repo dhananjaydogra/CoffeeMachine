6c2073c6eaacc11e7766ff388ce8276b2631f54f
dhananjaydogra <dhananjay27dogra@gmail.com>
2025-03-26 19:54:32 +0530

    Created same program with OOP concepts

---
+++ OOP-coffee-machine/main.py
@@ -0,0 +1,22 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+maker = CoffeeMaker()
+money = MoneyMachine()
+
+on = True
+
+while on:
+    order= input(f"What would you like? ({menu.get_items()[:-1]}): ")
+    if order == "off":
+        on = False
+    elif order == "report":
+        maker.report()
+        money.report()
+    elif menu.find_drink(order):
+        item=menu.find_drink(order)
+        if maker.is_resource_sufficient(item):
+            if money.make_payment(item.cost):
+                maker.make_coffee(item)
